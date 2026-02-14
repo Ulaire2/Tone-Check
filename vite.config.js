9afb924b82ce89358419e78263cadc5f651a39e4
Ulaire2 <15536012188@163.com>
2026-02-14 21:05:59 +0800

    style: update logo manually

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tone-Check/', // 必须与仓库名一致
+  base: '/',  // 
 })
